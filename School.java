dc5c7f7d845a1a02639d5c2b086a94706099cefe
SARAVANAN <148688977+Sara4Quantum@users.noreply.github.com>
2023-10-29 23:56:39 +0530

    Method Calling,  static and non, private modifier

---
+++ School.java
@@ -0,0 +1,19 @@
+package assignment;
+
+public class School 
+{  int mark;
+ private int salary;
+  static String school_name = "St. Antony's Primary School";
+
+  void conduct_exams()
+  {
+	 System.out.println("public exam"); 
+  }
+  void publish_results(int mark)
+  { 
+  System.out.println(mark);
+	  
+  }
+  
+}
+
---
+++ Teacher.java
@@ -0,0 +1,16 @@
+package assignment;
+
+public class Teacher {
+
+	public static void main(String[] args) {
+		School teacher = new School();
+		teacher.conduct_exams();
+		
+		teacher.publish_results(75);
+    
+	
+	System.out.println(teacher.school_name);
+	//System.out.println(teacher.salary);
+	}
+}
+
